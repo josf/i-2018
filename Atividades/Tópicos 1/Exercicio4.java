c471cc77491238aa794bb807e5a36f52f2401f74
Julien David <jdavidcastan@hotmail.com>
2018-04-02 20:38:29 -0300

    Update Exercicio4.java

--- Atividades/Tópicos 1/Exercicio4.java
+++ Atividades/Tópicos 1/Exercicio4.java
@@ -1,37 +1,49 @@
 package integracao2018;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class Exercicio4 {
 
 	public static void main(String[] args) throws IOException {
 
 		/*
-		 * Escreva um programa (linha de comandos) cujo primeiro argumento é o nome de
-		 * um arquivo e cujo segundo argumento é uma sequência de caracteres. Quando
-		 * executado, a sequência de caracteres fornecida como o segundo argumento é
-		 * persistida no arquivo cujo nome é fornecido como primeiro argumento. O texto
-		 * deve ser persistido no formato UTF-8.
+		 * Escreva um programa (linha de comandos) cujo primeiro argumento o
+		 * nome de um arquivo e cujo segundo argumento uma sequncia de
+		 * caracteres. Quando executado, a sequencia de caracteres fornecida
+		 * como o segundo argumento persistida no arquivo cujo nome e fornecido
+		 * como primeiro argumento. O texto deve ser persistido no formato
+		 * UTF-8.
 		 */
 
-		// Get the input file and data
-		File file = new File(args[0]);
-		FileInputStream fis = new FileInputStream(file);
-		StringBuffer sb = new StringBuffer();
+		String destFile = args[0];
+		String textArg = args[1];
+		String readText = null;
+		String lines;
+		FileInputStream fis = new FileInputStream(destFile);
+		InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		// Read all the text in the file
+		while ((lines = br.readLine()) != null) {
+			readText += lines;
+		}
 
-		try {
-			int ch;
-			while ((ch = fis.read()) != -1) {
-				sb.append((char) ch);
-			}
+		// Concat the printed text
+		try (PrintStream ps = new PrintStream(destFile, "UTF-8")) {
+			ps.println(textArg + readText);
 
-			fis.close();
-			System.out.println(sb);
+			// Flush the print stream
+			ps.flush();
 
-		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("Text has  been  written to " + (new File(destFile).getAbsolutePath()));
+		} catch (FileNotFoundException err) {
+			err.printStackTrace();
 		}
 
 	}
