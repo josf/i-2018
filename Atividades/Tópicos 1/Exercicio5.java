ea2ba5dd4b0c0d1e777e4b2605711bf6b530bd0e
Julien David <jdavidcastan@hotmail.com>
2018-04-02 21:47:17 -0300

    Add files via upload

---
+++ Atividades/Tópicos 1/Exercicio5.java
@@ -0,0 +1,49 @@
+package integracao2018;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * Escreva um programa (linha de comandos) que lê um arquivo texto e o
+		 * converte em um arquivo contendo uma sequência de bytes
+		 * correspondente. O arquivo texto (de entrada) é um texto codificado
+		 * como UTF-8.
+		 */
+
+		String destFile = args[0];
+		String textArg = args[1];
+		String readText = null;
+		String lines;
+		FileInputStream fis = new FileInputStream(destFile);
+		InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// Read all the text in the file
+		while ((lines = br.readLine()) != null) {
+			readText += lines;
+		}
+
+		// Concat the printed text
+		try (PrintStream ps = new PrintStream(destFile, "UTF-8")) {
+			ps.println(textArg + readText);
+
+			// Flush the print stream
+			ps.flush();
+
+			System.out.println("Text has  been  written to " + (new File(destFile).getAbsolutePath()));
+		} catch (FileNotFoundException err) {
+			err.printStackTrace();
+		}
+
+	}
+
+}
